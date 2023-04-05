e0c092b97632fb343b98107db0c8a55cb7421b25
Paulo Vitor <paulonovaes@id.uff.br>
2023-04-04 23:02:33 -0300

    criando classe de teste carrinho

---
+++ test/carrinho/CarrinhoTest.java
@@ -0,0 +1,24 @@
+package carrinho;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import produto.Produto;
+
+@DisplayName("Classe para teste do Carrinho")
+public class CarrinhoTest {
+	private static Carrinho car;
+	private static Produto p1;
+	private static Produto p2;
+	private static Produto p3;
+	
+	
+}
