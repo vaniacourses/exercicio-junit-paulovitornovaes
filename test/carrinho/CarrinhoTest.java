72dd2ab061a7542b4935395e171800198316b7b4
Paulo Vitor <paulonovaes@id.uff.br>
2023-04-05 21:22:16 -0300

    adicionando mais testes

--- test/carrinho/CarrinhoTest.java
+++ test/carrinho/CarrinhoTest.java
@@ -3,6 +3,7 @@ package carrinho;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import produto.Produto;
+import produto.ProdutoNaoEncontradoException;
 
 @DisplayName("Classe para teste do Carrinho")
 public class CarrinhoTest {
@@ -19,13 +21,14 @@ public class CarrinhoTest {
 	private static Produto p1;
 	private static Produto p2;
 	private static Produto p3;
-	
-	@BeforeAll
-	public static void inicializa() {
+	private static Produto p4;
+	@BeforeEach
+	public void inicializa() {
 		car = new Carrinho();
 		p1 = new Produto("maça", 5);
 		p2 = new Produto("uva", 10);
 		p3 = new Produto("pera", 15);
+		p4 = new Produto("fora do carrinho", 0);
 		car.addItem(p1);
 		car.addItem(p2);
 		car.addItem(p3);
@@ -39,10 +42,34 @@ public class CarrinhoTest {
 	
 	@Test
 	public void testValidaValorTotal() {
-		double valor_total = car.getValorTotal();
+		double valorTotal = car.getValorTotal();
 		
-		assertEquals(30, valor_total);
+		assertEquals(30, valorTotal);
 	}
+	
+	@Test
+	public void testRemoveProduto() {
+		try {
+			car.removeItem(p1);
+			int novaQuantidade = car.getQtdeItems();
+			assertEquals(2, novaQuantidade);
+		} catch (ProdutoNaoEncontradoException e) {
+			fail("não foi possivel remover o Item");
+		}
+	}
+	
+	@Test
+	public void testRemoveProdutoThrows() {
+		assertThrows(ProdutoNaoEncontradoException.class,
+				() -> car.removeItem(p4));
+	}
+	@Test
+	public void testEsvazia() {
+		car.esvazia();
+		var qtd = car.getQtdeItems();
+		assertEquals(qtd, 0);
+	}
+	
 	
 	
 }
